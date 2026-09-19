ffb9cabbb2cb59d53007306f65f3cc5bda21d8bf
ConnorL03 <leavittconnor23@gmail.com>
2026-09-19 14:06:17 -0600

    Finished prep assignment 2. Wrote logic to convert a string to and int. then wrote if else logic to update a string variable. Finally used that string variable in a F string to show students there letter grade.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,47 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage in CSE 210? ");
+        string grade_percent = Console.ReadLine ();
+        int grade_num = int.Parse(grade_percent);
+        string letter = "z";
+
+        if (grade_num >= 90)
+        {
+            letter = "A";
+        }
+        
+        else if (grade_num >= 80)
+        {
+            letter = "B";
+        }
+
+        else if (grade_num >= 70)
+        {
+            letter = "C";
+        }
+
+        else if (grade_num >= 60)
+        {
+            letter = "D";
+        }
+
+        else
+        {
+            letter = "F";
+        }
+        
+        Console.WriteLine($"Congrats you have a {letter} in the class");
+
+        if (grade_num >= 70)
+        {
+            Console.WriteLine("Congrats you are passing the class! Good Job!");
+        }
+
+        else
+        {
+            Console.WriteLine("You are not passing the class right now, keep turning things in to get your grade up!");
+        }
+
     }
 }
