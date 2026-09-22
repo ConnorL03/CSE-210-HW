584dd17731d6221f883b70f6f00b7754b3bb80f8
ConnorL03 <leavittconnor23@gmail.com>
2026-09-22 17:06:41 -0600

    Started by creating a list to hold all my varibles. Then created a do while loop to collect numbers from the user. At the end of that logic, I added an if statement to make sure 0 is not added to the list. I added a for each iterator to add all the varibles in my list. I wrote logic to find the average of all the varibles. Last I wrote a for each iterator in combination with an if statement to dittermain which is the highest numbers

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,43 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
+    
     {
-        Console.WriteLine("Hello Prep4 World!");
+        
+        List <int> numbers = new List<int> ();
+        int number = 1;
+        do
+        {
+            Console.Write("Enter a number: ");
+            number = int.Parse(Console.ReadLine());
+            if (number != 0)
+            {
+               numbers.Add (number);
+            }
+        } while (number != 0);
+
+        int total = 0;
+        foreach (int num in numbers)
+        {
+            total += num;
+        }
+        Console.WriteLine($"The sum is: {total}");
+
+        int average = total / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int highest_num = 0;
+        foreach (int num in numbers)
+        {
+            if (num> highest_num)
+            {
+                highest_num = num;
+            }
+        }
+        Console.WriteLine($"The largest number is: {highest_num}");
+
     }
 }
